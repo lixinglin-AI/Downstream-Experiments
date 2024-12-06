748d8d162979728682e681d89d496007188ad78c
lixinglin <linlixing_David@163.com>
2024-12-06 18:52:05 -0500

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python3 -u
-# Copyright (c) 2017-present, Facebook, Inc.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the LICENSE file in
-# the root directory of this source tree.
 from __future__ import print_function
 
 import argparse
